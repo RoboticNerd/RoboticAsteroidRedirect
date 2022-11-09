69577b7926eaf8e985d9f9b9269ad01d0744e115
RoboticNerd <franksebastianelli@outlook.com>
2022-11-09 13:50:57 -0800

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/fsebasti1/source/repos/RoboticAsteroidRedirect
+# Build directory: C:/Users/fsebasti1/source/repos/RoboticAsteroidRedirect/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
