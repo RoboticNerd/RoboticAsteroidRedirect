7ace28fa44b25aed1453a538737d4570fadc1730
RoboticNerd <franksebastianelli@outlook.com>
2022-11-13 12:15:45 -0800

    Verbosity flag included

--- constants.h
+++ constants.h
@@ -8,40 +8,42 @@
 //https://hanspeterschaub.info/Papers/UnderGradStudents/ConicReport.pdf
 
 // Gravitational Coefficients
-extern const double u_sun		= 1.32712440018e11;		// km^3/s^2
-extern const double u_earth		= 3.986004418e5;		// km^3/s^2
-extern const double u_moon		= 4.9028695e3;			// km^3/s^2
-extern const double u_jupiter   = 1.26686e8;            // km^3/s^2
+extern const double u_sun		    = 1.32712440018e11;		// km^3/s^2
+extern const double u_earth		    = 3.986004418e5;		// km^3/s^2
+extern const double u_moon		    = 4.9028695e3;			// km^3/s^2
+extern const double u_jupiter       = 1.26686e8;            // km^3/s^2
 
 // Orbital Radii
-extern const double r_orb_earth		    = 1.496e8;			// km wrt sun
-extern const double r_orb_moon		    = 3.85e6;			// km wrt earth
-extern const double r_orb_jupiter       = 7.786e8;          // km wrt sun
+extern const double r_orb_earth		= 1.496e8;			    // km wrt sun
+extern const double r_orb_moon		= 3.85e6;			    // km wrt earth
+extern const double r_orb_jupiter   = 7.786e8;              // km wrt sun
 
 
 // ASE Spacecraft Dynamics
 // https://solarsystem.nasa.gov/moons/earths-moon/in-depth/#:~:text=With%20a%20radius%20of%20about,miles%20(384%2C400%20kilometers)%20away.
 // Radii
-extern const double r_earth         = 6378;             // km
-extern const double r_moon          = 1740;             // km
+extern const double r_earth         = 6378;                 // km
+extern const double r_moon          = 1740;                 // km
 
 // Sphere of Influences
-extern const double soi_earth	= 9.29e5;				// km
-extern const double soi_moon	= 6.61e4;				// km
+extern const double soi_earth	    = 9.29e5;				// km
+extern const double soi_moon	    = 6.61e4;				// km
 
 // Typical GTO characteristics  https://conference.sdo.esoc.esa.int/proceedings/isdrw02/paper/10/ISDRW02-paper10.pdf
 extern const double a_GTO           = 24371;                // km - semimajor axis
 extern const double e_GTO           = 0.73;                 // - eccentricity
 extern const double r_a_GTO         = a_GTO*(e_GTO + 1);    // km - apoapsis
-extern const double r_p_GTO         = -a_GTO*(e_GTO - 1);    // km - periapsis
+extern const double r_p_GTO         = -a_GTO*(e_GTO - 1);   // km - periapsis
 // argument of perigee 180          // deg
 // orbital period 10.52             // hrs
 
 // Propulsion constants
-extern const double g_0         = 9.80665;              // m/s^2
-extern const double isp_RL10    = 465;                  // sec
-extern const double isp_NEXTC   = 4100;                 // sec
+extern const double g_0             = 9.80665;              // m/s^2
+extern const double isp_RL10        = 465;                  // sec
+extern const double isp_NEXTC       = 4100;                 // sec
 
+// SLS Vehicle Constants   https://www.lpi.usra.edu/lunar/constellation/SLS_FactSheet_long.pdf
+extern const double sls_payload_LEO = 130000;               // kg
 
 
 
--- main.cpp
+++ main.cpp
@@ -17,11 +17,11 @@ using namespace std;
 
 int graphtest();
 int graphingescapes();
-vector<double> asteroidReturnHohman(double m_asteroid, double r_orb_asteroid, double inert_fraction);
+vector<double> asteroidReturnHohman(double m_asteroid, double r_orb_asteroid, double inert_fraction, int verbosity);
 
 int debugi = 0;
 
-const double final_sc_drymass = 10;						// kg 		sc remaining mass on asteroid post burn
+const double final_sc_drymass = 10;						// kg 		sc remaining mass on asteroid post burn 					<---- Set
 
 
 int main() 
@@ -31,12 +31,14 @@ int main()
 	
 	// calculating arrival dv's ==============================================================
 	// initializations
-	double r_orb_asteroid 	= r_orb_jupiter;			// km 		asteroid radius
-	double m_asteroid 		= 1000;						// kg		asteroid mass
-	double inert_fraction = 0.20;						// 			inert fraction per stage of fuel mass
+	double r_orb_asteroid 	= r_orb_jupiter;			// km 		asteroid radius												<---- Set
+	double m_asteroid 		= 1000;						// kg		asteroid mass												<---- Set
+	double inert_fraction = 0.20;						// 			inert fraction per stage of fuel mass						<---- Set
 
 	vector<double> astVec;
-	astVec = asteroidReturnHohman(m_asteroid, r_orb_asteroid, inert_fraction);
+	astVec = asteroidReturnHohman(m_asteroid, r_orb_asteroid, inert_fraction, 1);
+
+	
 	
 
 
@@ -48,30 +50,30 @@ int main()
 
 
 
-vector<double> asteroidReturnHohman(double m_asteroid, double r_orb_asteroid, double inert_fraction){
+vector<double> asteroidReturnHohman(double m_asteroid, double r_orb_asteroid, double inert_fraction, int verbosity){
 
-	cout << "===========================================================" << endl;
+	if (verbosity == 1) {cout << "===========================================================" << endl;} 
 	double v_final = v_circ(u_earth, 10000);			// km/s		sc parking orbit velocity
 
 	double v_earth = v_circ(u_sun, r_orb_earth);		// km/s 	earth orbital velocity
 
 	double aaa = r_orb_asteroid + r_orb_earth;
 	double v_p_f = v_ellipse(u_sun, aaa, r_orb_earth);						// km/s  perigee arrival velocity
-	cout << "velocity of arrival ellipse perigee        : " << v_p_f << " km/s" << endl;
+	if (verbosity == 1) {cout << "velocity of arrival ellipse perigee        : " << v_p_f << " km/s" << endl;}
 
 	double v_inf_f = abs(v_p_f - v_earth);
-	cout << "parking circular velocity                  : " << v_inf_f << " km/s" << endl;
+	if (verbosity == 1) {cout << "parking circular velocity                  : " << v_inf_f << " km/s" << endl;}
 
 	double d_v_final = d_v_circle(v_final, v_inf_f);						// km/s 	dV needed to park in earth orbit
-	cout << "Arrival burn into parking orbit dV         : " << d_v_final << " km/s" << endl;
+	if (verbosity == 1) {cout << "Arrival burn into parking orbit dV         : " << d_v_final << " km/s" << endl;}
 
 	// Mass calculations now for last burn
 
 	double fuel_final = fuelMass(d_v_final*1000, final_sc_drymass+m_asteroid, isp_RL10);
 	double inert_final = fuel_final * inert_fraction;
 	fuel_final = fuelMass(d_v_final*1000, final_sc_drymass+m_asteroid+inert_final, isp_RL10);
-	cout << "final burn fuel mass " << fuel_final << " kg" << endl;
-	cout << "===========================================================" << endl;
+	if (verbosity == 1) {cout << "final burn fuel mass " << fuel_final << " kg" << endl;}
+	if (verbosity == 1) {cout << "===========================================================" << endl;}
 
 
 
@@ -80,41 +82,41 @@ vector<double> asteroidReturnHohman(double m_asteroid, double r_orb_asteroid, do
 	
 	// dv calculations for asteroid departure burn
 	double v_asteroid = v_circ(u_sun, r_orb_asteroid);	// km/s circular orbit as far as jupiter
-	cout << "Velocity of asteroid circular orbit        : " << v_asteroid << " km/s" << endl;
+	if (verbosity == 1) {cout << "Velocity of asteroid circular orbit        : " << v_asteroid << " km/s" << endl;}
 
 	double v_a_3 = v_ellipse(u_sun, aaa, r_orb_asteroid);			// km/s  apogee departure velocity
-	cout << "velocity of departure ellipse at apogee    : " << v_a_3 << " km/s" << endl;
+	if (verbosity == 1) {cout << "velocity of departure ellipse at apogee    : " << v_a_3 << " km/s" << endl;}
 
 	double d_v_3 = abs(v_asteroid - v_a_3);							// km/s  dV 3rd burn
-	cout << "dV of burn to move asteroid into ellipse   : " << d_v_3 << " km/s" << endl;
+	if (verbosity == 1) {cout << "dV of burn to move asteroid into ellipse   : " << d_v_3 << " km/s" << endl;}
 
 	// Mass calculations for 3rd burn
 	double mass_3 = fuel_final + inert_final + final_sc_drymass + m_asteroid;
-	cout << "mass after departing with asteroid         : " << mass_3 << " kg" << endl;
+	if (verbosity == 1) {cout << "mass after departing with asteroid         : " << mass_3 << " kg" << endl;}
 	double fuel_3 = fuelMass(d_v_3*1000, mass_3, isp_RL10);
 	double inert_3 = fuel_3 * inert_fraction;
 	fuel_3 = fuelMass(d_v_3*1000, mass_3+inert_3, isp_RL10);
-	cout << "third burn fuel mass " << fuel_3 << " kg" << endl;
-	cout << "===========================================================" << endl;
+	if (verbosity == 1) {cout << "third burn fuel mass " << fuel_3 << " kg" << endl;}
+	if (verbosity == 1) {cout << "===========================================================" << endl;}
 
 
 
 
 	// dv calculations for asteroid arrival burn
 	double v_a_2 = v_a_3;													// km/s  apogee departure velocity
-	cout << "Apogee velocity of arriving elipse         : " << v_a_2 << " km/s" << endl;
+	if (verbosity == 1) {cout << "Apogee velocity of arriving elipse         : " << v_a_2 << " km/s" << endl;}
 
 	double d_v_2 = abs(v_asteroid - v_a_2);									// km/s  dV 3rd burn
-	cout << "d_V of burn to join asteroid orbit         : " << d_v_2 << " km/s" << endl;
+	if (verbosity == 1) {cout << "d_V of burn to join asteroid orbit         : " << d_v_2 << " km/s" << endl;}
 
 	// Mass calculations for 2nd burn
 	double mass_2 = mass_3 + fuel_3 + inert_3 - m_asteroid;
-	cout << "mass after burning to join with asteroid   : " << mass_2 << " kg" << endl;
+	if (verbosity == 1) {cout << "mass after burning to join with asteroid   : " << mass_2 << " kg" << endl;}
 	double fuel_2 = fuelMass(d_v_2*1000, mass_2, isp_RL10);
 	double inert_2 = fuel_2*inert_fraction;
 	fuel_2 = fuelMass(d_v_2*1000, mass_2+inert_2, isp_RL10);
-	cout << "second burn fuel mass :" << fuel_2 << " kg" << endl;
-	cout << "===========================================================" << endl;
+	if (verbosity == 1) {cout << "second burn fuel mass :" << fuel_2 << " kg" << endl;}
+	if (verbosity == 1) {cout << "===========================================================" << endl;}
 
 
 
@@ -124,29 +126,30 @@ vector<double> asteroidReturnHohman(double m_asteroid, double r_orb_asteroid, do
 	// dv calculations for parabolic departure burn from earth
 	
 	double v_inf_1 = hyperbolicExcessSpeed(u_sun, r_orb_earth, r_orb_asteroid);
-	cout << "hyperbolic excess speed                    : " << v_inf_1 << " km/s" << endl;
+	if (verbosity == 1) {cout << "hyperbolic excess speed                    : " << v_inf_1 << " km/s" << endl;}
 	double v_esc_1 = escapeSpeed(u_earth, v_inf_1, r_earth+200); 																// using leo
 //	double v_esc_1 = escapeSpeed(u_earth, v_inf_1, r_p_GTO);          // 															FIX r orb departure
-	cout << "velocity needed to escape earth            : " << v_esc_1 << " km/s" << endl;										// using leo
+	if (verbosity == 1) {cout << "velocity needed to escape earth            : " << v_esc_1 << " km/s" << endl;}										// using leo
 	double v_p_1 = v_circ(u_earth, r_earth+200);
 //	double v_p_1 = v_ellipse(u_earth, a_GTO, r_p_GTO);
-	cout << "perigee velocity of departure ellipse      : " << v_p_1 << " km/s" << endl;
+	if (verbosity == 1) {cout << "perigee velocity of departure ellipse      : " << v_p_1 << " km/s" << endl;}
 	double d_v_1 = abs(v_esc_1 - v_p_1);
-	cout << "d_v of 1st burn                            : " << d_v_1 << " km/s" << endl;
+	if (verbosity == 1) {cout << "d_v of 1st burn                            : " << d_v_1 << " km/s" << endl;}
 
 
 	// Mass calculations for 1st burn
 	double mass_1 = fuel_2 + inert_2 + mass_2;
-	cout << "mass after burning to depart for asteroid  : " << mass_1 << " kg" << endl;
+	if (verbosity == 1) {cout << "mass after burning to depart for asteroid  : " << mass_1 << " kg" << endl;}
 	double fuel_1 = fuelMass(d_v_2*1000, mass_1, isp_RL10);
 	double inert_1 = fuel_1*inert_fraction;
 	fuel_1 = fuelMass(d_v_1*1000, mass_1+inert_1, isp_RL10);
-	cout << "1st burn fuel mass " << fuel_1 << " kg" << endl;
+	if (verbosity == 1) {cout << "1st burn fuel mass " << fuel_1 << " kg" << endl;}
 
 
 
 
-	// Final things
+	// Final things summary
+	if (verbosity == 1) {
 	cout << "===========================================================" << endl;
 	cout << "===========================================================" << endl;
 	cout << "Total spacecraft mass in LEO pre-burn      : " << fuel_1 + mass_1 + inert_1 << " kg" << endl;
@@ -155,7 +158,10 @@ vector<double> asteroidReturnHohman(double m_asteroid, double r_orb_asteroid, do
 	cout << "Asteroid recovered mass to 10,000km orbit  : " << m_asteroid << " kg" << endl;
  	cout << "===========================================================" << endl;
 	cout << "===========================================================" << endl;
+	}
 
+
+	// Returning final things
 	vector<double> returning;
 	returning.reserve(4);
 
